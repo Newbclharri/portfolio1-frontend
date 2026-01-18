51b4e9b1e0a39a9b27f975db973f7aa95269bc9f
contactharrisc2 <contactharrisc2@gmail.com>
2026-01-18 01:01:59 -0600

    Adds tutorials loading skeleton

--- src/pages/Tutorials.jsx
+++ src/pages/Tutorials.jsx
@@ -2,8 +2,13 @@ import { useState, useEffect } from "react";
 import YoutubeEmbed from "../components/YoutubeEmbed";
 
 export default function Tutorials(props) {
+
+    // isLoading state for testing loading skeletons
+    const [isLoading, setIsLoading] = useState(true);
+
     //set tutorials state variable
     const [tutorials, setTutorials] = useState(null);
+
     //create function to make api call
     const getTutorialsData = async () => {
         //make api call
@@ -12,26 +17,51 @@ export default function Tutorials(props) {
         console.log(data);
         setTutorials(data);
     }
-    //using useEffect so fxn runs just once when rendering page
-    useEffect(()=> {getTutorialsData()}, []);
 
-    const loaded = ()=>{
+    //using useEffect so fxn runs just once when rendering page
+    useEffect(() => { getTutorialsData() }, []);
+
+    const loaded = () => {
         //if data received, render with JSX
-        return tutorials.map(({title, url, id}, index)=>{
-            return(
+        return tutorials.map(({ title, url, id }, index) => {
+            return (
 
                 <div id="tutorials-container" key={title}>
-                    <h3 style={{width:"70%", margin: "0px auto", whiteSpace: "pre-line"}}>{title}</h3>
-                    <YoutubeEmbed embedId = {id} frameSize={props.frameSize}/>
-                    
+                    <h3 style={{ width: "70%", margin: "0px auto", whiteSpace: "pre-line" }}>{title}</h3>
+                    <YoutubeEmbed embedId={id} frameSize={props.frameSize} />
+
                     {/* Remove the last <hr> separator */}
                     {index !== tutorials.length - 1 && (
-                        <hr style={{width: "773px", margin: "0px auto"}} />
+                        <hr style={{ width: "773px", margin: "0px auto" }} />
                     )}
-                    <br/> {/**873 */}
+                    <br /> {/**873 */}
                 </div>
-            )        
-        })       
-    } 
-    return tutorials ? loaded() : <h1>Loading...</h1>
+            )
+        })
+    }
+
+    const loading = () => {
+        return (
+            <div>
+                <br />
+                {/* Skeleton tutorial cards */}
+                {[1, 2, 3].map((item) => (
+                    <div id="tutorials-container" key={item}>
+                        <div className="skeleton-text" style={{ width: "30%", height: "20px", margin: "0px auto" }}></div>
+                        <div className="skeleton-image" style={{ width: "40%", height: "400px", margin: "20px auto" }}>
+                            <div className="skeleton-vid-info">
+                                <div className="skeleton-text skeleton-text-short"></div>
+                                <div className="skeleton-text skeleton-text-short"></div>
+                                <div className="skeleton-text skeleton-text-short"></div>
+                            </div>
+                        </div>
+                        <hr style={{ width: "773px", margin: "0px auto" }} />
+                        <br />
+                    </div>
+                ))}
+            </div>
+        );
+    }
+
+    return tutorials ? loaded() : loading();
 }
